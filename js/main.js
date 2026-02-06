bb467462b695c178a59158058a7b99eaec8f373a
George-Mensah <qdanso11@gmail.com>
2026-02-06 08:51:43 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 const links = navLinks.querySelectorAll("a");
 
-function toggleMenu() {
+hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   document.body.classList.toggle("menu-open");
-}
+});
 
-hamburger.addEventListener("click", toggleMenu);
-
-// Close menu when a link is clicked
+// Close menu when clicking a link
 links.forEach(link => {
   link.addEventListener("click", () => {
     navLinks.classList.remove("show");
@@ -19,10 +17,12 @@ links.forEach(link => {
 
 // Close menu when clicking outside
 document.addEventListener("click", function(e) {
-  if (navLinks.classList.contains("show") && !navLinks.contains(e.target) && e.target !== hamburger) {
+  if (
+    navLinks.classList.contains("show") &&
+    !navLinks.contains(e.target) &&
+    e.target !== hamburger
+  ) {
     navLinks.classList.remove("show");
     document.body.classList.remove("menu-open");
   }
 });
-
-
